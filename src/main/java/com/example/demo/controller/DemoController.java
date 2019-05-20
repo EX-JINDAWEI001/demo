693ac3402047fe19f9fbdc7ead42e3a862051aa8
金大为 <david@jindaweideMacBook-Air.local>
2019-05-20 12:01:29 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class DemoController {
+
+    private Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @RequestMapping("/test.do")
+    public String test1(HttpServletRequest request, HttpServletResponse response) {
+        logger.info("test1 input params:{}", JSON.toJSONString(request.getParameterMap()));
+        return "HELLO WORLD !!!";
+    }
+
+}
