374877407630ed69b05d9e93d7fd0afb69a5d0d6
EX-JINDAWEI001 <15971565197@163.com>
2019-05-23 15:49:19 +0800

    TEST GITHUB

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/demo")
 @RestController
@@ -23,6 +29,51 @@ public class DemoController {
     @RequestMapping("/test2.do")
     public String test2(HttpServletRequest request, HttpServletResponse response) {
         return "TEST GITHUB !!!";
+    }
+
+    @RequestMapping("/getAppLoadingList.do")
+    public Map<String, Object> getAppLoadingList(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //路径
+        System.out.println("contextPath:" + request.getContextPath());
+        System.out.println("servletPath:" + request.getServletPath());
+        System.out.println("pathInfo:" + request.getPathInfo());
+        System.out.println("pathTranslated:" + request.getPathTranslated());
+        System.out.println("realPath:" + request.getServletContext().getRealPath("/getAppLoadingList.do"));
+        System.out.println("RequestURI:" + request.getRequestURI());
+        System.out.println("RequestURL:" + request.getRequestURL());
+
+        //参数
+        System.out.println("ParameterValues:" + request.getParameterValues("appId"));
+        System.out.println("ParameterMap:" + JSON.toJSONString(request.getParameterMap()));
+        System.out.println("HeaderNames:" + JSON.toJSONString(request.getHeaderNames()));
+//		System.out.println("Parts:" + request.getParts());
+
+        //cookie
+        System.out.println("cookies:" + request.getCookies());
+
+        //语言环境
+        System.out.println("locale:" + request.getLocale());
+        System.out.println("locales:" + JSON.toJSONString(request.getLocales()));
+
+        //servletContext
+        ServletContext sc = request.getServletContext();
+        sc.setAttribute("jdw", "jindawei");
+        System.out.println("" + sc.getAttribute("jdw"));
+
+        response.sendError(10, "唧唧复唧唧");
+        //异步
+        request.startAsync();
+        AsyncContext ac = request.getAsyncContext();
+        System.out.println(ac.hasOriginalRequestAndResponse());
+        System.out.println(request.isAsyncSupported());
+        System.out.println(request.isAsyncStarted());
+        ac.setTimeout(1000);
+        System.out.println(ac.getTimeout());
+        ac.complete();
+        String appId = request.getParameter("appId");
+        Map map = new HashMap();
+        map.put("jdw", appId);
+        return map;
     }
 
 }
