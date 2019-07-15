f5a27c8cf700be98ea2f8ddc8e566573212908ad
EX-JINDAWEI001 <15971565197@163.com>
2019-07-15 17:50:34 +0800

    TEST GITHUB

--- src/main/java/com/example/demo/common/job/DemoJob.java
+++ src/main/java/com/example/demo/common/job/DemoJob.java
@@ -1,14 +1,20 @@
 package com.example.demo.common.job;
 
+import com.example.demo.common.mongo.MongoLockHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoJob {
 
+    @Autowired
+    private MongoLockHandler mongoLockHandler;
+
     @Scheduled(cron = "0/5 * * * * ?")
     public void doJob() {
         System.out.println("demoJob doJob------------");
+        System.out.println(mongoLockHandler.getLock("test", 1000));
     }
 
 }
---
+++ src/main/java/com/example/demo/common/mongo/MongoLockHandler.java
@@ -0,0 +1,69 @@
+package com.example.demo.common.mongo;
+
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+public class MongoLockHandler {
+
+    @Resource
+    private MongoTemplate mongoTemplate;
+
+    public boolean getLock(String key, long expire) {
+        List<MongoLock> locks = this.getLockByKey(key);
+        if (locks.size() > 0) {
+            if (locks.get(0).getExpire() >= System.currentTimeMillis()) {//锁已经被别人获取
+                return false;
+            } else {//释放过期的锁
+                this.releaseExpiredLock(key, System.currentTimeMillis());
+            }
+        }
+
+        int value = this.upsertLock(key, 1, System.currentTimeMillis() + expire);
+        return value == 1 ? true : false;
+
+    }
+
+    public void releaseLock(String key) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("key").is(key));
+        mongoTemplate.remove(query, MongoLock.class);
+    }
+
+    private Integer upsertLock(String key, int value, long expireTime) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("key").is(key));
+
+        Update update = new Update();
+        update.inc("value", value);
+        update.set("expire", expireTime);
+
+        FindAndModifyOptions options = new FindAndModifyOptions();
+        options.upsert(true);   //存在则更新,否则插入
+        options.returnNew(true); //返回更新后的值
+
+        MongoLock mongoLock = mongoTemplate.findAndModify(query, update, options, MongoLock.class);
+        return mongoLock.getValue();
+    }
+
+    private void releaseExpiredLock(String key, long expireTime) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("key").is(key));
+        query.addCriteria(Criteria.where("expire").lt(expireTime));
+        mongoTemplate.remove(query, MongoLock.class);
+    }
+
+    private List<MongoLock> getLockByKey(String key) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("key").is(key));
+        return mongoTemplate.find(query, MongoLock.class);
+    }
+
+}
