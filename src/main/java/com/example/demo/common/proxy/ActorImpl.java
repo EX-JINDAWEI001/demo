efd0fcdcf35ddf308880283ec1fbb2d0087e85db
EX-JINDAWEI001 <15971565197@163.com>
2019-10-10 21:48:07 +0800

    TEST GITHUB

---
+++ src/main/java/com/example/demo/common/proxy/ActorImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.common.proxy;
+
+public class ActorImpl implements Actor {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello Proxy World !");
+    }
+}
---
+++ src/main/java/com/example/demo/common/proxy/dynamicProxy/DynamicProxy.java
@@ -0,0 +1,42 @@
+package com.example.demo.common.proxy.dynamicProxy;
+
+import com.example.demo.common.proxy.Actor;
+import com.example.demo.common.proxy.ActorImpl;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class DynamicProxy implements InvocationHandler {
+
+    private Object realClass;
+
+    public DynamicProxy(Object realClass){
+        super();
+        this.realClass = realClass;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("invoke start...");
+        Object invoke = method.invoke(realClass, args);
+        System.out.println("invoke end...");
+        return invoke;
+    }
+
+    /**
+     * 动态代理测试
+     * @param args
+     */
+    public static void main(String[] args) {
+        Actor actor = new ActorImpl();
+        Actor dynamicProxy = (Actor) Proxy.newProxyInstance(
+                actor.getClass().getClassLoader(),
+                actor.getClass().getInterfaces(),
+                new DynamicProxy(actor)
+        );
+
+        dynamicProxy.sayHello();
+    }
+
+}
