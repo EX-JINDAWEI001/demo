31711ae38d205b06485280661cc0c82f1ef4e7f9
jindawei <15971565197@163.com>
2019-10-23 18:43:23 +0800

    some

--- src/main/java/com/example/demo/algorithm/Sort.java
+++ src/main/java/com/example/demo/algorithm/Sort.java
@@ -7,20 +7,20 @@ public class Sort {
     public static void bubbleSort(Integer[] arr) {
         int length = arr.length;
         int temp = 0;
-        for (int i=0; i<length-1; i++){
-            for (int j=0; j<length-1-i; j++){
-                if (arr[j] < arr[j+1]) {
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
                     temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            System.out.println("第" + (i+1) + "趟：" + Arrays.asList(arr));
+            System.out.println("第" + (i + 1) + "趟：" + Arrays.asList(arr));
         }
     }
 
     public static void main(String[] args) {
-        Integer[] arr = new Integer[]{12,34,32,1,3,76,98,54,31};
+        Integer[] arr = new Integer[]{12, 34, 32, 1, 3, 76, 98, 54, 31};
         bubbleSort(arr);
     }
 
