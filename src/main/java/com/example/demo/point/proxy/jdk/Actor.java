18db9bb58180f38ba35597210399b6c5497742cc
ex-jidnawei001 <15971565197@163.com>
2019-12-23 15:10:52 +0800

    pattern

--- src/main/java/com/example/demo/point/proxy/jdk/Actor.java
+++ src/main/java/com/example/demo/point/proxy/jdk/Actor.java
@@ -3,7 +3,18 @@ package com.example.demo.point.proxy.jdk;
 public interface Actor {
     void sayHello();
 
-    default void hello(String in){
+    // JDK8 默认方法
+    default void hello(String in) {
         System.out.println(in);
+    }
+
+    // JDK8 静态方法
+    static void blowHorn() {
+        System.out.println("按喇叭!!!");
+    }
+
+    // JDK9 私有方法
+    private void run() {
+        System.out.println("跑起来!!!");
     }
 }
