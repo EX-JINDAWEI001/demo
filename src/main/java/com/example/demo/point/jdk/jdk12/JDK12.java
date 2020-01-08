298b0beba69492741f4ccae75a6d9ec027f12e6d
ex-jidnawei001 <15971565197@163.com>
2020-01-08 17:30:15 +0800

    commit

--- src/main/java/com/example/demo/point/jdk/jdk12/JDK12.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.point.jdk.jdk12;
-
-/**
- * JDK12新特性记录
- */
-public class JDK12 {
-    
-}
--- src/main/java/com/example/demo/point/jdk/jdk13/JDK13.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.point.jdk.jdk13;
-
-/**
- * JDK13新特性记录
- */
-public class JDK13 {
-    
-}
